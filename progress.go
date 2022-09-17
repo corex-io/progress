bec84af441d7a111afcdfa7f3964353b59985cef
luopengift <870148195@qq.com>
2022-09-17 13:42:49 +0800

    update display

--- progress.go
+++ progress.go
@@ -11,12 +11,11 @@ import (
 
 // Progress 进图结构体
 type Progress struct {
-	now      time.Time
-	options  Options
-	current  int64 // 开始
-	isFinish bool
-	mutex    sync.Mutex
-	once     sync.Once
+	now     time.Time
+	options Options
+	current int64 // 开始
+	mutex   sync.Mutex
+	once    sync.Once
 }
 
 // New 初始化方法
@@ -30,41 +29,75 @@ func New(opts ...Option) *Progress {
 }
 
 // Add 增加进度
-func (progress *Progress) Add(i int64) {
+func (progress *Progress) Add(i int64) *Progress {
 	progress.mutex.Lock()
 	defer progress.mutex.Unlock()
 	progress.current += i
+	return progress
 }
 
 func (progress *Progress) Print() {
 	progress.mutex.Lock()
 	defer progress.mutex.Unlock()
-	if progress.isFinish {
-		return
-	}
+
+	display := []string{progress.options.Content, "[", "", "]", "0.00%", "0/0", "(0s)"}
+	// 0: content  1: 开始符号  2: 进度显示  3: 结束符号  4: 进度  5: 当前/总量  6: 时间
+
 	width := 80
 	width, _, _ = term.GetSize(int(os.Stdout.Fd()))
-	width = width*1/3 - 30 - len(progress.options.Content)
 
 	percent := float64(progress.current) / float64(progress.options.End)
 
-	rate := strings.Repeat(progress.options.Graph, int(percent*100*float64(width)/float64(progress.options.End)))
-	_, _ = fmt.Fprintf(os.Stderr, "\r%s[%-"+fmt.Sprintf("%d", width)+"s] %.2f%% %8d/%d (%.2fs)", progress.options.Content, rate, percent*100, progress.current, progress.options.End, time.Since(progress.now).Seconds())
+	display[4] = fmt.Sprintf("%7.3f%%", percent*100)
+	display[5] = fmt.Sprintf("%10d/%-10d", progress.current, progress.options.End)
+	display[6] = fmt.Sprintf("%6s", Human(time.Since(progress.now)))
+	width -= len(display[0]) + len(display[1]) + len(display[3]) + len(display[4]) + len(display[5]) + len(display[6])
+	//width = width * 4 / 5
+	display[2] = fmt.Sprintf("%-"+fmt.Sprintf("%d", width)+"s", strings.Repeat(progress.options.Graph, int(percent*100*float64(width)/float64(progress.options.End))))
+
+	_, _ = fmt.Fprintf(os.Stderr, "\r%s", strings.Join(display, ""))
 	// %-50s 左对齐, 占50个字符位置, 打印string
 	// %3d   右对齐, 占3个字符位置 打印int
-	if progress.options.End <= progress.current {
+	if progress.IsFinish() {
 		progress.Finish()
 	}
 }
 
 // IsFinish return progress is finish or not.
 func (progress *Progress) IsFinish() bool {
-	return progress.isFinish
+	return progress.current >= progress.options.End
 }
 
 func (progress *Progress) Finish() {
 	progress.once.Do(func() {
-		progress.isFinish = true
 		_, _ = fmt.Fprintf(os.Stderr, "\n")
 	})
+}
+
+func Human(d time.Duration) string {
+	ss := []struct {
+		dis string
+		time.Duration
+	}{
+		{"y", time.Hour * 24 * 365},
+		{"d", time.Hour * 24},
+		{"h", time.Hour},
+		{"m", time.Minute},
+		{"s", time.Second},
+		{"ms", time.Millisecond},
+	}
+	var b strings.Builder
+	i := 0
+	for _, post := range ss {
+		if d >= post.Duration {
+			t := d / post.Duration
+			fmt.Fprintf(&b, "%d%s", t, post.dis)
+			d -= t * post.Duration
+			i += 1
+		}
+		if i == 3 {
+			return b.String()
+		}
+	}
+	return b.String()
 }
--- progress_test.go
+++ progress_test.go
@@ -1,19 +1,34 @@
 package progress
 
 import (
+	"fmt"
 	"testing"
 	"time"
 )
 
 func TestRun(t *testing.T) {
-	p := New(Content("测试1: "))
+	p := New(Content("测试1111111111: "))
 
 	for {
-		time.Sleep(100 * time.Millisecond)
+		time.Sleep(1000 * time.Millisecond)
 		p.Add(int64(2))
 		p.Print()
 		if p.IsFinish() {
 			return
 		}
 	}
+}
+
+func TestLen(t *testing.T) {
+	c := fmt.Sprintf("%7.3f", 82.3)
+	fmt.Println(c, len(c))
+	d := fmt.Sprintf("%10d/%d", 1, 2)
+	fmt.Println(d, len(d))
+	e := time.Since(time.Now().Add(-4 * time.Second))
+	ee := fmt.Sprintf("%6s", e)
+	fmt.Println(ee, len(ee))
+	day := time.Minute * 60 * 24
+	year := 365 * day
+	fmt.Println(Human(time.Duration(12*year + 365*day + 5*day + 5*time.Hour + 1998*time.Millisecond)))
+
 }
