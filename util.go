d63b91397759c7e279eca6ae5ab127f406ab1cca
luopengift <870148195@qq.com>
2022-09-19 01:17:57 +0800

    update

--- util.go
+++ util.go
@@ -20,17 +20,15 @@ var tt = []struct {
 
 func HumanTime(d time.Duration) string {
 	var b strings.Builder
-	i := 0
+	i, t := 0, time.Duration(0)
 	for _, post := range tt {
 		if d >= post.Duration {
-			i += 1
-			t := d / post.Duration
+			i, t = i+1, d/post.Duration
 			if _, _ = fmt.Fprintf(&b, "%d%s", t, post.unit); i == 3 {
 				return b.String()
 			}
 			d = d % post.Duration
 		}
-
 	}
 	return b.String()
 }
